a03f8f341cbad0bf9d30fbebfb96680974cccfe2
pellu <jordanpelluard@gmail.com>
2017-04-19 18:29:12 +0200

    ajax good

--- header.php
+++ header.php
@@ -13,6 +13,7 @@
     <link href="<?= $racines; ?>css/bootstrap.min.css" rel="stylesheet">
     <link href="<?= $racines; ?>css/style.css" rel="stylesheet">
     <link href="<?= $racines; ?>css/profil.css" rel="stylesheet">
+    <link href="<?= $racinea; ?>css/jquery-ui.css" rel="stylesheet">
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
